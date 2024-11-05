ea28faef4852335e6cf9dc5c96dd743615ca36a4
this-is-yosuke <yosuke.kibe@gmail.com>
2024-11-05 15:54:21 -0700

    Updated server.ts
    Added middleware for parsing JSON and encoded data. Serving static files will have to wait until I generate the dist directory.

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,9 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-
+// I NEED TO GENERATE THE DIST FOLDER FIRST. RESOLVE HTMLROUTES ERRORS BEFORE THAT
+app.use(express.static(''));
 // TODO: Implement middleware for parsing JSON and urlencoded form data
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 // TODO: Implement middleware to connect the routes
 app.use(routes);
 
