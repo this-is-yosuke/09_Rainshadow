118a5bc48d305b323137b41ba1ec39c1ded81570
this-is-yosuke <yosuke.kibe@gmail.com>
2024-11-08 00:14:58 -0700

    Updated weathRoutes.ts
    Changed the post method. Changed the get method.

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -7,34 +7,27 @@ import WeatherService from '../../service/weatherService.js';
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', async (req: Request, res: Response) => {
   // TODO: GET weather data from city name
-  const { city } = req.body;
-  if(!city){
-    return res.status(400).json({ error: 'City cannot be blank'});
-  }
   try {
-    const weatherData = await WeatherService.getWeatherForCity(city);
-    // -------------------------------------------------------------------------------
-      // router.get('/city/:name', (req, res) => {
-        // try {
-        //   const cityName = req.params.name;
-        //   const 
-        // }
-    
-      // });
-    // ------------------------------------------------------------------------------
+    const cityName = req.body.cityName;
+    WeatherService.getWeatherForCity(cityName).then((data) => {
       // TODO: save city to search history
-      await HistoryService.addCity(city);
-      return res.status(200).json(weatherData);
-  } catch (err) {
-    return res.status(500).json( { error: 'Unable to obtain weather data'} );
+      HistoryService.addCity(cityName);
+      res.json(data);
+    });
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
 // TODO: GET search history
 router.get('/history', async (_req: Request, res: Response) => {
   try {
-    const savedCities = await HistoryService.getCities();
-    res.json(savedCities);
+    // const savedCities = await HistoryService.getCities();
+    // res.json(savedCities);
+    HistoryService.getCities()
+    .then((data) => {
+      return res.json(data);
+    })
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
